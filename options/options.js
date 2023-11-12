89f6835447f8d5d50d67477733e859cbe9fa7f4c
Crowdbotics <team@crowdbotics.com>
2023-11-12 21:27:17 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "marijuana-island-44501",
+  url: "https://marijuana-island-44501.botics.co",
+  api: "https://marijuana-island-44501.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
